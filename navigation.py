e9eb5034ec8b570743f9daced61f792ee3031df7
Cory Johns <cory@triplevisiongroup.com>
2019-04-16 00:29:05 -0600

    erosion working not done with path vector

--- navigation.py
+++ navigation.py
@@ -6,8 +6,6 @@ class PathDetection:
 
     def __init__(self):
         cv.namedWindow("Video")
-        cv.namedWindow("Canny")
-        pass
 
     def get_path(self, frame):
         h, w = frame.shape[:2]
@@ -25,9 +23,23 @@ class PathDetection:
         filled_from_bottom = np.zeros((h, w), np.uint8)
         filled_from_bottom[inds_after_edges] = 255
         # erode
-        filled_from_bottom = cv.erode(filled_from_bottom, (500, 500))
+        filled_from_bottom = cv.erode(filled_from_bottom, np.ones((25, 25)))
         # Smooth Hull
-        # hull = cv.convexHull(filled_from_bottom)
+        # find direction
+        contours, _ = cv.findContours(filled_from_bottom, mode=cv.RETR_TREE, method=cv.CHAIN_APPROX_SIMPLE)
+        cv.drawContours(frame, contours, -1, (255, 0, 0))
+        hull = []
+        area_hull = []
+        for i in range(len(contours)):
+            hull.append(cv.convexHull(contours[i], False))
+            area_hull.append(cv.contourArea(contours[i]))
+        hull = np.array(hull)
+        cv.drawContours(frame, hull, -1, (0, 255, 0))
+        # get max hull and use that
+        area_max_index = np.argmax(area_hull)
+        width_points = np.argpartition(-hull[area_max_index][:,:,0].ravel(), 2)
+        cv.drawContours(frame, hull[area_max_index], -1, (0, 0, 255), thickness=2)
+
         tmp = cv.add(np.zeros(frame.shape, np.uint8), 100*np.ones(frame.shape, np.uint8), mask=filled_from_bottom)
         frame = cv.add(tmp, frame)
         cv.imshow("Video", frame)
