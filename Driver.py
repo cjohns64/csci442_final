6f1db768df772297101990059a686c4356cf5e18
Cory Johns <cory@triplevisiongroup.com>
2019-04-17 11:39:49 -0600

    get path is working, basic movement outline:
    - still lacking any kind of state transitions

--- Driver.py
+++ Driver.py
@@ -1,55 +1,48 @@
 import cv2 as cv
-import numpy as np
 import navigation as nav
 
-cap = cv.VideoCapture(0)
-cv.namedWindow("Video")
 
-cap.set(cv.CAP_PROP_FRAME_WIDTH, 400)
-cap.set(cv.CAP_PROP_FRAME_HEIGHT, 300)
-# get video info
-status, frame = cap.read()
-width, height, channel = frame.shape
+class Driver:
 
+    def __init__(self):
+        # set up video capture
+        self.cap = cv.VideoCapture(0)
+        cv.namedWindow("Video")
 
-def draw_blob(img, verts):
-    """
-    Draws a line around a area given by the list of vertexes, given in the weird OpenCV list of [[x, y]]
-    :param img: the image to draw the blob on
-    :param verts: the bounds of the blob given in the order around the blob
-    :return: None
-    """
-    for i in range(len(verts) - 1):
-        v1 = tuple(verts[i][0])
-        v2 = tuple(verts[i+1][0])
-        cv.line(img, v1, v2, (255, 0, 0), 3)
-    cv.line(img, tuple(verts[-1][0]), tuple(verts[0][0]), (255, 0, 0), 3)
+        # set screen size
+        self.cap.set(cv.CAP_PROP_FRAME_WIDTH, 400)
+        self.cap.set(cv.CAP_PROP_FRAME_HEIGHT, 300)
 
+        # set up objects
+        self.navigation_obj = nav.PathDetection(display=True)
+        self.move_function = None
 
-def bounding_box(verts):
-    """
-    Returns the bounding box for a blob given as a list of vertexes as a list of [[x, y]]
-    the returned list of vertexes of the bounding box are also in this form.
-    :param verts: the bounds of the blob given in the order around the blob
-    :return: the bounds of the bounding box
-    """
-    min_x, min_y = np.min(verts[:,0], axis=0)
-    max_x, max_y = np.max(verts[:,0], axis=0)
+    def obstacle_avoidance_state(self, frame):
+        """
+        State for navigating through the obstacles
+        :param frame: the current frame of the camera
+        :return:
+        """
+        if frame is not None:
+            # get path target location on the screen
+            nav_x, nav_y = self.navigation_obj.get_path(frame)
+            # get the movement function we need
+            self.move_function = self.navigation_obj.get_rotation(nav_x - frame.shape[1] // 2)
+            # TODO use the movement function
+            pass
 
-    return np.array([[[min_x, min_y]], [[max_x, min_y]], [[max_x, max_y]], [[min_x, max_y]]])
+    def main_loop(self):
+        while True:
+            # get video info
+            status, frame = self.cap.read()
 
+            # TODO transition between states
+            self.obstacle_avoidance_state(frame)
 
-x = nav.PathDetection()
+            cv.imshow("Video", frame)
 
-while True:
-    # get video info
-    status, frame = cap.read()
-
-    if frame is not None:
-        x.get_path(frame)
-
-    k = cv.waitKey(1)
-    # this is the "esc" key
-    if k == 27:
-        break
-cv.destroyAllWindows()
+            k = cv.waitKey(1)
+            # this is the "esc" key
+            if k == 27:
+                break
+        cv.destroyAllWindows()
--- navigation.py
+++ navigation.py
@@ -4,16 +4,22 @@ import cv2 as cv
 
 class PathDetection:
 
-    def __init__(self):
-        cv.namedWindow("Video")
+    def __init__(self, display=False):
+        self.display = display
 
     def get_path(self, frame):
+        """
+        Gets the target direction from the given frame
+        :param frame: the current camera frame
+        :return: target pixel location, the location on the screen for the highest unobstructed path
+        """
         h, w = frame.shape[:2]
+        # default target to the center bottom of the screen
+        target_point = (w//2, h)
         # Gaussian Blur
         blur = cv.GaussianBlur(frame, (3, 3), cv.BORDER_DEFAULT)
         # convert to gray scale
         blur = cv.cvtColor(blur, cv.COLOR_BGR2GRAY)
-        # Floor Finder?
         # Canny
         cv.Canny(blur, 125, 255, blur)
         # vertical fill
@@ -24,22 +30,73 @@ class PathDetection:
         filled_from_bottom[inds_after_edges] = 255
         # erode
         filled_from_bottom = cv.erode(filled_from_bottom, np.ones((25, 25)))
-        # Smooth Hull
-        # find direction
+        filled_from_bottom = cv.dilate(filled_from_bottom, np.ones((25, 25)))
+
+        # find the contours of the path area
         contours, _ = cv.findContours(filled_from_bottom, mode=cv.RETR_TREE, method=cv.CHAIN_APPROX_SIMPLE)
-        cv.drawContours(frame, contours, -1, (255, 0, 0))
+        if self.display:
+            cv.drawContours(frame, contours, -1, (255, 0, 0))
         hull = []
         area_hull = []
+        # get convex hulls and their areas for all the contours
         for i in range(len(contours)):
             hull.append(cv.convexHull(contours[i], False))
             area_hull.append(cv.contourArea(contours[i]))
+        # convert to numpy array
         hull = np.array(hull)
-        cv.drawContours(frame, hull, -1, (0, 255, 0))
-        # get max hull and use that
-        area_max_index = np.argmax(area_hull)
-        width_points = np.argpartition(-hull[area_max_index][:,:,0].ravel(), 2)
-        cv.drawContours(frame, hull[area_max_index], -1, (0, 0, 255), thickness=2)
+        if self.display:
+            cv.drawContours(frame, hull, -1, (0, 255, 0))
 
-        tmp = cv.add(np.zeros(frame.shape, np.uint8), 100*np.ones(frame.shape, np.uint8), mask=filled_from_bottom)
-        frame = cv.add(tmp, frame)
-        cv.imshow("Video", frame)
+        # get max hull and use that
+        if len(hull) > 0:
+            area_max_index = np.argmax(area_hull)
+            # get the highest two points
+            width_points = np.argpartition(hull[area_max_index][:,:,1].ravel(), 2)
+            width_points = [hull[area_max_index][width_points[0]], hull[area_max_index][width_points[1]]]
+            if self.display:
+                # draw the two target points
+                cv.drawContours(frame, np.array(width_points), -1, (0, 0, 255), thickness=10)
+            # remove ugly cv formatting
+            width_points = [list(width_points[0][0,:]), list(width_points[1][0,:])]
+            # get target point
+            target_point = ((width_points[0][0] + width_points[1][0]) // 2, width_points[0][1])
+            if self.display:
+                # display target point
+                cv.line(frame, (w // 2, h), target_point, (0, 0, 255))
+
+        if self.display:
+            # display path area
+            tmp = cv.add(np.zeros(frame.shape, np.uint8), 100*np.ones(frame.shape, np.uint8), mask=filled_from_bottom)
+            frame = cv.add(tmp, frame)
+            cv.imshow("Video", frame)
+        # return target point
+        return np.array(target_point)
+
+    def get_rotation(self, target_x_from_center, min_action_value=40):
+        """
+        Gets whether the robot should rotate or not and in what direction
+        :param target_x_from_center: the target horizontal location as measured from the center of the screen
+        :param min_action_value: the minimum number of pixels the target must be away from the center for a rotation
+        to be needed
+        :return: the movement function needed
+        """
+        if np.abs(target_x_from_center) < min_action_value:
+            return self.move_forward  # no rotation
+        elif target_x_from_center > 0:
+            # rotate left
+            return self.rotate_left  # rotate left
+        else:
+            # rotate right
+            return self.rotate_right  # rotate right
+
+    def move_forward(self):
+        # TODO add motor movement
+        pass
+
+    def rotate_right(self):
+        # TODO add motor movement
+        pass
+
+    def rotate_left(self):
+        # TODO add motor movement
+        pass
