a6758b9db882b37efd857c73e57290fe800ba8e2
Cory Johns <cory@triplevisiongroup.com>
2019-04-22 12:13:41 -0600

    Added pi_cam_loop

--- Driver.py
+++ Driver.py
@@ -1,6 +1,15 @@
 import cv2 as cv
 from StateControl import StateController
+from client import ClientSocket
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 
+IP = '10.200.22.237'
+PORT = 5010
+face = None
+# set up client and face searching
+client = ClientSocket(IP, PORT)
 
 class Driver:
 
@@ -30,8 +39,39 @@ class Driver:
                 break
         cv.destroyAllWindows()
 
+    @ staticmethod
+    def pi_cam_loop(obj):
+        # initialize the camera and grab a reference to the raw camera capture
+        camera = PiCamera()
+        cv.namedWindow("Video")
+        w, h = 320, 240
+        camera.resolution = (w, h)  # (640, 480)
+        camera.framerate = 32
+        rawCapture = PiRGBArray(camera, size=camera.resolution)
+        # capture frames from the camera
+        for image in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+            # grab the raw NumPy array representing the image, then initialize the timestamp
+            # and occupied/unoccupied text
+            frame = image.array
+
+            # run one frame of the main operating loop
+            if obj.main_loop_step(frame):
+                # cycle complete
+                break
+
+            cv.imshow("Video", frame)
+
+            key = cv.waitKey(1) & 0xFF
+            # clear the stream in preparation for the next frame
+            rawCapture.truncate(0)
+
+            # if the `q` key was pressed, break from the loop
+            if key == ord("q"):
+                obj.zero_motors()
+                break
+
 
 # start robot state object
 robot = StateController(debug=True)
 # run w/ laptop camera
-Driver.laptop_cam_loop(robot)
+Driver.pi_cam_loop(robot)
