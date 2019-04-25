a1ea73224cd706f60361e83a95f6ee3e024a6e10
John Lambrecht <jclambrecht97@msn.com>
2019-04-25 17:31:05 -0600

    shoulder_side added to movements

--- Navigation.py
+++ Navigation.py
@@ -17,10 +17,12 @@ class Navigation:
         self.HEADTILT = 4
         self.ELBOW = 8
         self.SHOULDER = 6
+        self.SHOULDER_SIDE = 7
         self.HAND = 11
         self.hand = 6000
         self.elbow = 6000
         self.shoulder = 6000
+        self.shoulder_side = 7000#TODO 5400
         self.body = 6000
         self.headTurn = 6000
         self.headTilt = 6000
@@ -233,8 +235,11 @@ class Navigation:
     def arm_raise(self):
         if self.debug: print("arm raised without releasing hand")
         self.shoulder = 7500
+        self.shoulder_side = 5400
         if not laptop:
             self.tango.setTarget(self.SHOULDER, self.shoulder)
+            time.sleep(.75)
+            self.tango.setTarget(self.SHOULDER_SIDE, self.shoulder_side)
 
     def arm_grab_ice(self):
         if self.debug: print("arm grabbed ice")
