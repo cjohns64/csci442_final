b3a6d174e494c5d3018016d4e867b9b6768f5829
Cory Johns <cory@triplevisiongroup.com>
2019-04-28 17:34:49 -0600

    burst

--- Navigation.py
+++ Navigation.py
@@ -235,7 +235,7 @@ class Navigation:
             for value in range(self.slow_lower_value, self.fast_lower_value, -self.motor_step):
                 self.turn = value
                 if not laptop: self.tango.setTarget(self.TURN, self.turn)
-                time.sleep(0.2)
+                time.sleep(0.4)
             self.zero_wheels()
 
     def turn_180(self):
