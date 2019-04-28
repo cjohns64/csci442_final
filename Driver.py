f7f39c2974552b2981da2dfb91338cb1b8da8c67
Cory Johns <cory@triplevisiongroup.com>
2019-04-28 14:39:27 -0600

    h-w to w-h in pixel access

--- Driver.py
+++ Driver.py
@@ -110,7 +110,7 @@ class Driver:
                 h, w = frame.shape[:2]
                 frame = cv.GaussianBlur(frame, (9, 9), cv.BORDER_DEFAULT)
                 if sampling:
-                    color = frame[w//2, h//2]
+                    color = frame[h//2, w//2]
                     cv.circle(frame, (w // 2, h // 2), 3, (0, 0, 255), 5)
                     try:
                         wi, hi, loc = obj.find_color_in_frame(frame, color)
@@ -145,7 +145,7 @@ class Driver:
 
                 # cv.setMouseCallback('picture', get_bgr, param=frame)
 
-                print("BGR", frame[w//2, h//2])
+                print("BGR", frame[h//2, w//2])
                 cv.imshow("Video", frame)
 
                 key = cv.waitKey(1) & 0xFF
