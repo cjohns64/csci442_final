077988ae417ee84932a842c5a70e47460c4a3991
John Lambrecht <jclambrecht97@msn.com>
2019-04-29 18:23:15 -0600

    New turn mult

--- Navigation.py
+++ Navigation.py
@@ -39,7 +39,7 @@ class Navigation:
 
         # motor values
         self.forward_boost_mult = 4
-        self.rotate_boost_mult = 1
+        self.rotate_boost_mult = 6
         self.boost_delta = 100
         self.slow_forward = 5200
         self.slow_right = 5000
