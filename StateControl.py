50ad7b0b1043b78369bc9bae43f777e8835545b1
John Lambrecht <jclambrecht97@msn.com>
2019-04-29 20:20:42 -0600

    Changed mining area standard

--- StateControl.py
+++ StateControl.py
@@ -76,9 +76,9 @@ class StateController:
         # and the ratio of the current sensor value and this distance
         # will be compared to the distance_ratio to determine if we have reached the target or not
         self.face_width_standard = 50
-        self.mining_area_standard = 55
+        self.mining_area_standard = 65
         self.goal_small_standard = 135
-        self.goal_large_standard = 120
+        self.goal_large_standard = 130
         self.goal_size_standard = self.goal_small_standard
         self.start_size_standard = self.goal_size_standard - 40
         # color standard values based off of sampling
