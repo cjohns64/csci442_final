75a30d9cc6e1743b21765fa72294ea3c2951e29b
John Lambrecht <jclambrecht97@msn.com>
2019-04-30 11:46:49 -0600

    pink calibrated, cal off

--- StateControl.py
+++ StateControl.py
@@ -89,6 +89,7 @@ class StateController:
         self.pink_standard_520 = [165, 150, 225] # pink for 5:20 p.m.
         self.pink_standard_915 = [157, 76, 253] # pink for 9:15 a.m.
         self.pink_standard_1010 = [160, 85, 200] # pink for 10:10 a.m.
+        self.pink_standard_1145 = [155, 40, 253] # pink for 11:45 a.m.
         self.use_pink = self.pink_standard_1010
 
         self.green_standard = [50, 170, 150]  # BGR [25, 200, 110]
