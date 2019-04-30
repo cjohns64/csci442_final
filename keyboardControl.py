952eebf04824a7e2773e7666802d0b781b232fab
John Lambrecht <jclambrecht97@msn.com>
2019-04-30 11:26:05 -0600

    Arm

--- keyboardControl.py
+++ keyboardControl.py
@@ -71,16 +71,17 @@ class KeyControl():
             print('waist left')
         elif key.keycode == 53:
             # self.elbow+=200
-            # self.shoulder_side-=200
+            self.shoulder_side-=200
+            print (self.shoulder_side)
             # print(self.elbow)
-            self.shoulder-=200
-            print(self.shoulder)
+            # self.shoulder-=200
+            # print(self.shoulder)
             #self.hand += 200
             # print(self.hand)
             # self.tango.setTarget(HAND, self.hand)
-            # self.tango.setTarget(SHOULDER_SIDE, self.shoulder_side)
+            self.tango.setTarget(SHOULDER_SIDE, self.shoulder_side)
             # self.tango.setTarget(ELBOW, self.elbow)
-            self.tango.setTarget(SHOULDER,self.shoulder)
+            # self.tango.setTarget(SHOULDER,self.shoulder)
    
     
     def arrow(self, key):
